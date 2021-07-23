43657b921ceca8617b00101751dcd7da92b5a7dc
Wil688 <wilfriedkouame@hotmail.fr>
2021-07-23 12:10:37 +0200

    Admin configuration

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+        /**
+        * @Route("/home", name="home")
+        */
+        public function index(): Response
+        {
+            return $this->render('home.html.twig');
+        }
+
+        /**
+        * @Route("/admin", name="admin")
+        */
+        public function admin()
+        {
+            $users = $this->getDoctrine()->getRepository(User::class)->findBy(
+                [],
+                //['lastUpdateDate' => 'DESC']
+            );
+        
+            $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+        
+            return $this->render('default/admin.html.twig', [
+                //'car' => $car,
+                //'users' => $users
+            ]);
+        }
+        
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -194,4 +194,14 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function addRoles(string $roles): self
+    {
+        if (!in_array($roles, $this->roles)) {
+            $this->roles[] = $roles;
+        }
+    
+        return $this;
+    }
+    
 }
